3bf5f89883ba06ba057f9516353d991b453eb271
ronandrevon <ronandrevon@gmail.com>
2020-02-15 16:29:51 +0000

    updated setup.py for dependencies so it can be distributed

--- setup.py
+++ setup.py
@@ -19,4 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'matplotlib','numpy','scipy','colorama',],
 )

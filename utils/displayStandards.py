616b3d213c9d9e9678e65094b9f3401691a8539f
ronandrevon <ronandrevon@gmail.com>
2020-02-26 10:51:16 +0000

    added stddisp as a better version of stdDispPlt, includes scatter plot. added createFig to take care of monitor size etc. Prevent setAxPos by default as pad option for plt.tight_layout seems to be doing a better job.

--- utils/displayStandards.py
+++ utils/displayStandards.py
@@ -1,14 +1,27 @@
-import matplotlib;
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RectangleSelector
 import numpy as np
 from .glob_colors import*
+from subprocess import check_output
+
+# Get screen info, remove toolbar
+#matplotlib.rcParams['toolbar'] = 'None'
+dpi = check_output("xdpyinfo | awk '/resolution/{print $2}'",shell=True).decode()
+dpi = np.array(dpi.strip().split('x'),dtype=int)
+screen_size = check_output("xrandr | grep \"*+\" | awk '{print $1}'",shell=True).decode().split('\n')
+#choose second monitor if present
+screen_size=screen_size[int(len(screen_size)>2)]
+screen_size = np.array(screen_size.split('x'),dtype=int)/dpi #inches
+
+
 ###########################################################################
 #### def: plotting standards 
-def standardDisplay(ax,labs,name='', xylims=[], axPos=[],legOpt=1,
+def standardDisplay(ax,labs=['',''],name='', xylims=[], axPos=[],legOpt=1,
                     fonts=[30,25,15,20], c=['k','k'], logOpt='',changeXYlims=True,
                     gridOn=True,ticksOn=True,title='',legLoc='upper left',legElt=[],
-                    xyTicks=[],xyTickLabs=[[],[]],opt='',setPos=True):
+                    xyTicks=[],xyTickLabs=[[],[]],mg=0.05,opt='',setPos=False,equal=False):
+    ''' opt : p(plot), s(save), c(close) '''
     axPos = get_axPos(axPos); #print(axPos)
     ax.name = name; 
     if isinstance(fonts,dict) : fonts = get_font(fonts,dOpt=True)
@@ -24,12 +37,48 @@ def standardDisplay(ax,labs,name='', xylims=[], axPos=[],legOpt=1,
     #position,grid,title,legend,save
     if setPos : ax.set_position(axPos)
     if gridOn : ax.grid(gridOn,color=(0.9,0.9,0.9),linestyle='-')
+    if equal : ax.axis('equal')
     ax.set_axisbelow(True)
-    if changeXYlims : changeAxesLim(ax,0.05, xylims,xyTicks,xyTickLabs)
-    plt.axis(['off','on'][ticksOn])
+    ax.axis(['off','on'][ticksOn])
+    if changeXYlims : 
+        changeAxesLim(ax,mg, xylims,xyTicks,xyTickLabs)
     ax.set_title(title, {'fontsize': fsT}); #print(title)
     addLegend(ax,fsLeg,legOpt,legLoc,legElt)
     disp_quick(name,ax,opt)
+
+def create_fig(figsize=(0.5,1),pad=None) : 
+    '''figsize : 
+        tuple : (width,height) normalized units
+        str   : f(full),12(half),22(quarter)
+    '''
+    if isinstance(figsize,str) : 
+        figsize = {'f':(1,1),'12':(0.5,1),'22':(0.5,0.5)}[figsize]
+    figsize = tuple(np.array(figsize)*screen_size)
+    fig,ax = plt.subplots(figsize=figsize,dpi=dpi[0])
+    #fig.canvas.manager.window.move(px,py)
+    if pad : plt.tight_layout(pad)
+    return fig,ax
+
+def stddisp(plots=[],scat=[],texts=[],colls=[],axPos=[],
+            lw=1,ms=5,marker='o',fonts={},
+            ax=None,fig=None,figsize=(0.5,1),pad=None,
+            **kwargs):
+    '''
+    fonts.keys=['lab','leg','tick','text','title']
+    fonts,lw,ms,maker : 
+    '''
+    if isinstance(fonts,dict) : fonts = get_font(fonts)
+    fsT = fonts[3]; fonts=(np.array(fonts)[[0,1,2,4]]).tolist()
+    #if showOpt and not 'q' in opt : opt+='p'
+    if not ax : fig,ax = create_fig(figsize,pad)
+    if ax and axPos==[] : axPos=ax.get_position().bounds
+    #add lines,patches,texts,scatters
+    for coll in colls : ax.add_collection(coll)
+    pltScatter(ax,scat,ms,marker)
+    pltPlots(ax,plots,lw,ms)
+    pltTexts(ax,texts,fsT)
+    standardDisplay(ax,axPos=axPos,fonts=fonts,opt=opt,**kwargs)
+    return fig,ax
 
 def stdDispPlt(plots,xlabs=['',''],name='',xlims=[],axPos=[],c=['k','k'],
                showOpt=1,ax=None,fig=None,texts=[],legOpt=1,lw=1,ms=5,fonts={},
@@ -80,6 +129,7 @@ def get_font(d_font=dict(),dOpt=False) :
     fonts = [ font_dict[k] for k in keys]
     return  fonts
 
+########################################################################
 #### def : utils 
 def pltPlots(ax,plots,lw0,ms0=5):
     if len(plots)==4:
@@ -95,7 +145,24 @@ def pltTexts(ax,texts,fsT):
     for t in texts:
         c = 'k' if len(t)<4 else t[3]
         ax.text(t[0],t[1],t[2],fontsize=fsT,color=c)
-        
+
+def pltScatter(ax,scat,s=5,marker='o') : 
+    '''scat : [x,y,s,c]
+            : [x,y,c] 
+           s : int or list/np.array of int
+           c : tuple,str or list/np.array of tuple/str'''
+    if len(scat) : 
+        if len(scat)==4:
+            x,y,s,c = scat
+        elif len(scat)==3:
+            x,y,c = scat
+        N=np.array(x).size
+        if isinstance(s,int) : 
+            s = [s]*N
+        if isinstance(c,tuple) or isinstance(c,str) : 
+            c = [c]*N
+        ax.scatter(x,y,s,c,marker=marker)
+
 def getCML(C):
     ml = ''
     if isinstance(C,list):
@@ -470,6 +537,18 @@ def testGetSymCurve():
     x2,y2 = getSymCurve(x2,y2,-1)
     stdDispPlt([[x1,y1,'b','$cos$'],[x2,y2,'r','$sin$']])
 
+def test_stddisp() : 
+    x,y = np.random.rand(2,10)
+    c   = ['b']*10
+    s   = [5]*10
+    ct  = [(0.2,0.3,0.1)]*10
+    stddisp(scat=[x,y,s,c]          ,opt='q',legOpt=0)
+    stddisp(scat=[x,y,s[0],c[0]]    ,opt='q',legOpt=0)
+    stddisp(scat=[x,y,c]            ,opt='q',legOpt=0)
+    stddisp(scat=[x,y,np.array(c)]  ,opt='q',legOpt=0)
+    stddisp(scat=[x,y,ct]           ,opt='q',legOpt=0)
+    stddisp(scat=[x,y,ct[0]]        ,opt='p',legOpt=0)
+
 if __name__=='__main__':
     #testHandler()
     #testPrint()
@@ -480,9 +559,10 @@ if __name__=='__main__':
     #testStdDispPlt_c()
     #testStdDispPlt_subplots()
     #testAddxAxis()
-    #testAddyAxis()    
+    #testAddyAxis()
     #testChangeAxLim()
     #print getCML(np.random.rand(3))
     #testGetSymCurve()
     #test_get_axPos()
-    print('displayStandards compiled')
+    #test_stddisp()
+    print(green+'displayStandards compiled'+black)
